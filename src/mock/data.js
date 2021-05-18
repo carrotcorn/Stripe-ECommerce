11c30c009176a1277f501e1d2841ec36942d8893
Eric Bourne <ericbourne6371@gmail.com>
2021-05-17 17:45:41 -0700

    adding to mock data file

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,3 +1,16 @@
 import { nanoid } from "nanoid"
 
+// HEAD DATA
+export const headData = {
+  title: "Gatsby Stripe E-commerce dummy site",
+  lang: "en",
+  description:
+    "Dummy store for utilizing for future e-commerce sites utilizing stripe checkout",
+  //   ogImg: "",
+}
 
+//HOME DATA
+export const homeData = {
+  storeName: "Sea To Sky Cycles",
+  headerOne: "Sale Items",
+}
